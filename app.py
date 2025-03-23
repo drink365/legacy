a5fa341e445d2ff812bce9ba56c9506dc2f531f1
drink365 <166272199+drink365@users.noreply.github.com>
2025-03-23 21:44:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -151,15 +151,12 @@ if st.session_state.module_four_done:
     st.markdown("🪴 模組四完成 ✔️")
 
     st.markdown("""
-🌿 恭喜您，已完成所有階段！  
-這些思考，將是未來傳承藍圖的起點。
+🌿 恭喜您，這些思考將是未來傳承藍圖的起點。
 """)
 
     st.markdown("""
 📩 如果您想更具體地展開行動，  
 我們誠摯邀請您預約 30 分鐘專屬對談。  
-這是一場未來的開端，一次為自己和家族的對話機會。  
-
 📌 永傳家族辦公室｜[https://gracefo.com/](https://gracefo.com/)  
 📧 Email｜[123@gracefo.com](mailto:123@gracefo.com)
 """)

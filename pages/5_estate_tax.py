15bbdbf1bac3cb4c51d78fa37ef31217cd41a9ab
drink365 <166272199+drink365@users.noreply.github.com>
2025-03-31 14:42:03 +0800

    Update 5_estate_tax.py

--- pages/5_estate_tax.py
+++ pages/5_estate_tax.py
@@ -25,7 +25,7 @@ render_estate_tax_ui(calculator)
 # 最下方資訊
 st.markdown("---")
 st.markdown("""
-<div style='text-align: center; font-size: 12px; color: gray;'>
+<div style='text-align: center; font-size: 14px; color: gray;'>
 永傳家族辦公室｜<a href="https://gracefo.com" target="_blank">https://gracefo.com</a><br>
 聯絡信箱：<a href="mailto:123@gracefo.com">123@gracefo.com</a>
 </div>

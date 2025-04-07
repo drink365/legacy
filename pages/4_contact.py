d090139dc2030cca395f61592b7f279399dd82ed
drink365 <166272199+drink365@users.noreply.github.com>
2025-04-07 23:30:41 +0800

    Update 4_contact.py

--- pages/4_contact.py
+++ pages/4_contact.py
@@ -6,30 +6,41 @@ st.set_page_config(
     layout="centered"
 )
 
+# 頁首標題
 st.markdown("""
 <div style='text-align: center;'>
-    <h2>📬 聯絡我們</h2>
-    <p style='font-size: 16px;'>歡迎與《影響力》團隊聯繫，我們樂意協助您進一步了解平台服務或安排一對一傳承對談。</p>
+    <h1 style='font-size: 36px;'>📬 聯絡我們</h1>
+    <p style='font-size: 16px; color: gray;'>歡迎與《影響力》團隊聯繫，我們樂意陪伴您思考、設計屬於自己的傳承策略。</p>
 </div>
 """, unsafe_allow_html=True)
 
+# 聯絡資訊區塊
+st.markdown("---")
 st.markdown("""
----
-
-### 📧 聯絡信箱
+### 📧 電子信箱  
+若您有任何疑問，或想預約一對一對談，請來信：  
 <a href="mailto:123@gracefo.com">123@gracefo.com</a>
 
-### 🌐 官方網站
+### 🌐 官方網站  
+更多關於我們的介紹與服務內容，歡迎造訪：  
 <a href="https://gracefo.com" target="_blank">https://gracefo.com</a>
 
-### 📌 公司名稱
-永傳家族辦公室  
-永傳科創股份有限公司
-
-### 📍 公司地址
-台北市中山區南京東路二段101號9樓
+### 📌 公司資訊  
+永傳家族辦公室｜永傳科創股份有限公司  
+台北市中山區南京東路二段 101 號 9 樓
 
 ---
 
-我們重視每一位用戶的提問與回饋，期待為您提供最貼近需求的傳承規劃協助。
+我們重視每一位用戶的提問與回饋，  
+期盼成為您在傳承旅程中的陪伴者與策略夥伴。
+""", unsafe_allow_html=True)
+
+# 頁尾統一識別
+st.markdown("---")
+st.markdown("""
+<div style='text-align: center; font-size: 14px; color: gray;'>
+《影響力》傳承策略平台｜永傳家族辦公室  
+<a href="https://gracefo.com" target="_blank">https://gracefo.com</a><br>
+聯絡信箱：<a href="mailto:123@gracefo.com">123@gracefo.com</a>
+</div>
 """, unsafe_allow_html=True)
